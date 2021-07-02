4f92e6d5c8b6eda436eadbb90c9ce2a0e98f6890
Ergin TIRAVOGLU <ergintiravoglu@msn.com>
2021-07-02 23:31:27 +0300

    Story List

--- app.js
+++ app.js
@@ -1,11 +1,53 @@
+const path = require('path')
 const express = require('express')
+const mongoose = require('mongoose')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
+const passport = require('passport')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const connectDB = require('./config/db')
+//const { param } = require('./routes/index')
 
 //Load config
 dotenv.config({ path: './config/config.env'})
 
+//Passport config
+require('./config/passport')(passport)
+
+connectDB()
+
 const app = express()
 
+if(process.env.NODE_ENV === 'development')
+{
+    app.use(morgan('dev'))
+}
+
+
+//Handlebars
+app.engine('.hbs', exphbs({defaultLayout:'main', extname: '.hbs'}))
+app.set('view engine', '.hbs')
+
+//Sessions
+app.use(session({
+    secret:'keyboardcar',
+    resave:false,
+    saveUninitialized:false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection})
+}))
+
+//Pasport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+//Static Folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+//Routes
+app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
 const PORT = process.env.PORT || 5000
 
 
